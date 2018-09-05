1e1404939435b9071de4cf7a5e45a07298b80bdb
Ram <Ram@LAPTOP-F9LMSVH5>
2018-09-05 21:39:37 +0530

    Demo is updated...

--- src/com/app/Demo.java
+++ src/com/app/Demo.java
@@ -2,8 +2,12 @@ package com.app;
 
 public class Demo {
 
-	
+	static void show()
+	{
+		System.out.println("This is Show...");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is Main method...");
+		show();
 	}
 }
